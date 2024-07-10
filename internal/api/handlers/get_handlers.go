31259ec038ff9e47750a54fa9f7c452b85274ba5
Fredrik Svanholm <fredrik.svanholm@gmail.com>
2024-07-10 21:22:37 +0200

    Refactor accountId validation
    - Moved out param validation to seperate function

--- internal/api/handlers/get_handlers.go
+++ internal/api/handlers/get_handlers.go
@@ -8,6 +8,18 @@ import (
 	"github.com/justfredrik/bank-api/internal/db"
 )
 
+func validateAccountIdParam(c *gin.Context) (uint64, error) {
+	id, err := strconv.ParseUint(c.Param("accountId"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   "Bad Request",
+			"message": "id is not a positive integer",
+		})
+		c.Abort()
+	}
+	return id, err
+}
+
 func GetPing(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "pong",
@@ -16,13 +28,10 @@ func GetPing(c *gin.Context) {
 
 func GetAccount(c *gin.Context) {
 
-	// validate id param format
-	id, err := strconv.ParseUint(c.Param("accountId"), 10, 64)
+	// validate accountId param format
+	id, err := validateAccountIdParam(c)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error":   "Bad Request",
-			"message": "id is not a positive integer",
-		})
+		return
 	}
 
 	// Fetch Account from mock db
@@ -52,4 +61,24 @@ func GetAccounts(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, accounts)
+}
+
+func GetTransaction(c *gin.Context) {
+
+	// validate accountId param format
+	accountId, err := validateAccountIdParam(c)
+	if err != nil {
+		return
+	}
+
+	// Fetch Account from mock db
+	transaction, err := db.DB.GetTransaction(accountId, c.Param("transactionId"))
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Not Found", "message": "transaction not found"})
+		return
+	}
+
+	// Return valid account data
+	c.JSON(http.StatusOK, *transaction)
+
 }
