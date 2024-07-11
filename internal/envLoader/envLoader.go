9715c2b2d0be8740f840bcc3e3973ad6f72045de
Fredrik Svanholm <fredrik.svanholm@gmail.com>
2024-07-11 15:29:37 +0200

    Update envLoader.go

--- internal/envLoader/envLoader.go
+++ internal/envLoader/envLoader.go
@@ -6,8 +6,10 @@ var loaded = false
 
 func init() {
 	// Load enviorenment varialbes
-	if err := godotenv.Load(); err != nil {
-		panic(err)
+	if err := godotenv.Load("../.env"); err != nil {
+		if err2 := godotenv.Load(".env"); err2 != nil {
+			panic(err2)
+		}
 	}
 	loaded = true
 }
