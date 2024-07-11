510eb7cfd0bc6dbe7a6a683eeee347b661b49d29
Fredrik Svanholm <fredrik.svanholm@gmail.com>
2024-07-11 19:36:53 +0200

    Remove unnecessary comments

--- internal/api/handlers/get_handlers.go
+++ internal/api/handlers/get_handlers.go
@@ -28,20 +28,17 @@ func GetPing(c *gin.Context) {
 
 func GetAccount(c *gin.Context) {
 
-	// validate accountId param format
 	id, err := validateAccountIdParam(c)
 	if err != nil {
 		return
 	}
 
-	// Fetch Account from mock db
 	acc, err := db.DB.GetAccount(id)
 	if err != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": "Not Found", "message": "account not found"})
 		return
 	}
 
-	// Return valid account data
 	c.JSON(http.StatusOK, *acc)
 
 }
@@ -49,7 +46,6 @@ func GetAccount(c *gin.Context) {
 func GetAccounts(c *gin.Context) {
 	// No support for pagination but would be good to have if in real prod
 
-	// Fetch Accounts from mock db
 	accounts, err := db.DB.GetAccounts(0, 0)
 	if err != nil {
 		c.JSON(http.StatusNotFound, gin.H{
@@ -64,7 +60,6 @@ func GetAccounts(c *gin.Context) {
 
 func GetTransaction(c *gin.Context) {
 
-	// validate accountId param format
 	accountId, err := validateAccountIdParam(c)
 	if err != nil {
 		// This should technically be unreachable since this has already been validated in the AUTH step.
@@ -72,21 +67,18 @@ func GetTransaction(c *gin.Context) {
 		return
 	}
 
-	// Fetch Transaction from mock db
 	transaction, err := db.DB.GetAccountTransaction(accountId, c.Param("transactionRef"))
 	if err != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": "Not Found", "message": "transaction not found"})
 		return
 	}
 
-	// Return valid transaction data
 	c.JSON(http.StatusOK, *transaction)
 
 }
 
 func GetTransactions(c *gin.Context) {
 	// No support for pagination but would be good to have if in real prod
-	// validate accountId param format
 	accountId, err := validateAccountIdParam(c)
 	if err != nil {
 		// This should technically be unreachable since this has already been validated in the AUTH step.
@@ -94,14 +86,12 @@ func GetTransactions(c *gin.Context) {
 		return
 	}
 
-	// Fetch transactions from mock db
 	transactions, err := db.DB.GetAccountTransactions(accountId)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error", "message": "the server was uanble to fetch the transactions"})
 		return
 	}
 
-	// Return valid transactions data
 	c.JSON(http.StatusOK, transactions)
 
 }
